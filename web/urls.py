f9f55092573f800d90a822d70c684238b8069d64
sumin <suminvarghese5@gmail.com>
2023-01-08 13:09:54 +0530

    msg

---
+++ web/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import *
+urlpatterns = [
+    path("register/",SignUpView.as_view(),name="signup"),
+    path("login/",SignInView.as_view(),name="signin"),
+    path('index/',IndexView.as_view(),name="index" ),
+    path("posts/<int:id>/comment/add",add_comment,name="add-comment"),
+    path("post/<int:id>/like/add",like_post_view,name="add-like"),
+    path('logout',signout_view,name="sign-out"),
+    path("profile/", Profile.as_view(), name='profile'),
+    path("user/<int:id>/follower/add", add_follower, name="add-follower"),
+    path("user/<int:id>/note/add", add_notes, name="add-note"),
+    path("people/", ListPeopleView.as_view(), name="people"),
+    path("comment/<int:id>/remove",comment_delete,name="comment-delete"),
+    path("add/profile",AddProfile.as_view(),name="addprofile")
+
+]
